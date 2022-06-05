119fae91b3830b5d9ba5748070c98da79bc8fba8
Peking Duck <pekingduck@users.noreply.github.com>
2022-06-05 13:04:09 +0800

    Made use of IParser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BlockArguments        #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiWayIf            #-}
 {-# LANGUAGE OverloadedStrings     #-}
 
 module Main where
@@ -23,14 +24,19 @@ data SomeID =
 
 data Food = Sushi | Tofu deriving (Show, Eq)
 
-readFood :: Reader Food
-readFood =
-  \case
-    "Sushi" -> Right (Sushi, "")
-    "Tofu"  -> Right (Tofu, "")
-    s       -> Left "Invalid food"
+food :: Reader Food
+food str =
+  case matchText "Sushi" str <> matchText "Tofu" str <> Left "" of
+    Right ("Sushi", b) -> Right (Sushi, b)
+    Right ("Tofu", b)  -> Right (Tofu, b)
+    _                  -> Left "Invalid Food"
+  -- if | T.isPrefixOf "Sushi" s -> go Sushi "Sushi" s
+  --    | T.isPrefixOf "Tofu" s  -> go Tofu "Tofu" s
+  --    | otherwise              -> Left "Invalid food"
+  -- where
+  --   go a pre s = Right (a, T.drop (T.length pre) s)
 
-parseFood key result = fst <$> parse readFood key result
+parseFood key result = fst <$> parse (P food <* P eos) key result
 
 main :: IO ()
 main = do
@@ -39,28 +45,29 @@ main = do
     l = fromJust (mkListWithDefault (Just Sushi) (Sushi NL.:| [Tofu]))
     imageWidth = fromJust $ mkPixel 250
     b :: [Widget SomeID]
-    b = [ button OtherButton "Don't Click!"
-        , (comboBox Combo ("Two" NL.:| [ "Worlds", "Collide" ]))
-          { completion = Just CaseSensitive }
-        , radioButton Radio l
-        , (openBrowser Browser)
-          { fileType = Just (Extensions ("jpg" NL.:| ["dhall"])) }
-        , (saveBrowser Save)
-          { fileExtension = Just "cabal" }
-        , popup Pop l
-        , (defaultButton OkButton) { label_ = Just "Subscribe" }
-        , (textField TxtField) { default_ = Just "Black Friday" }
-        , (password Passwd) { label_ = Just "Tell me your password" }
-        , (date Cal) { choice = Just DateOnly
-                            , default_ = Just "1997-07-01"
-                            , style = Just Textual }
-        , (text_ Txt "OK\nOK") { tooltip = Just "Tip of the iceberg"
-                               , relY = mkRelY (negate 20) }
-        , (textBox TxtBox) { default_ = Just "Hmm\nmumm"
-                                  , disabled = Just False
-                                  , fontType = Just Fixed }
-        , (image Img "test.jpg") { dimensions = Just (Width imageWidth Nothing)
-                                 , relY = mkRelY 300 }
+    b = [ radioButton Radio l
+    --  button OtherButton "Don't Click!"
+    --     , (comboBox Combo ("Two" NL.:| [ "Worlds", "Collide" ]))
+    --       { completion = Just CaseSensitive }
+
+    --     , (openBrowser Browser)
+    --       { fileType = Just (Extensions ("jpg" NL.:| ["dhall"])) }
+    --     , (saveBrowser Save)
+    --       { fileExtension = Just "cabal" }
+    --     , popup Pop l
+    --     , (defaultButton OkButton) { label_ = Just "Subscribe" }
+        , (textField TxtField) { label_ = Just "Enter a number" }
+    --     , (password Passwd) { label_ = Just "Tell me your password" }
+    --     , (date Cal) { choice = Just DateOnly
+    --                         , default_ = Just "1997-07-01"
+    --                         , style = Just Textual }
+    --     , (text_ Txt "OK\nOK") { tooltip = Just "Tip of the iceberg"
+    --                            , relY = mkRelY (negate 20) }
+    --     , (textBox TxtBox) { default_ = Just "Hmm\nmumm"
+    --                               , disabled = Just False
+    --                               , fontType = Just Fixed }
+    --     , (image Img "test.jpg") { dimensions = Just (Width imageWidth Nothing)
+    --                              , relY = mkRelY 300 }
         ]
     w = window { title = Just "One Weird Trick"
                       , transparency = Just 0.9
@@ -69,3 +76,4 @@ main = do
   result <- runPashua f
   print result
   print $ parseFood Radio result
+  print $ parseInt TxtField result
--- src/Graphics/UI/Pashua/Parser.hs
+++ src/Graphics/UI/Pashua/Parser.hs
@@ -1,53 +1,92 @@
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Graphics.UI.Pashua.Parser
   ( Err(..)
+  , IR.IParser(..)
+  , R.Reader
+  , R.decimal
+  , R.rational
+  , R.signed
+  , R.hexadecimal
+  , R.double
+  , takeAll
+  , matchText
+  , bool
+  , eos
   , parseText
   , parseFloat
   , parseBool
   , parseInt
   , assoc
   , parse
-  , R.Reader
   ) where
 
-import           Data.Bifunctor     (first)
-import           Data.List          (find)
-import           Data.Text          (Text, head, tail)
-import           Data.Text.Read     as R
-import           Graphics.UI.Pashua (Result (..))
-import           Prelude            hiding (head, tail)
+import           Data.Bifunctor          (first)
+import           Data.List               (find)
+import           Data.Text
+    ( Text
+    , drop
+    , head
+    , isPrefixOf
+    , length
+    , tail
+    )
+import           Data.Text.Internal.Read as IR
+import           Data.Text.Read          as R
+import           Graphics.UI.Pashua      (Result (..))
+import           Prelude                 hiding (drop, head, length, tail)
 
 data Err a = FormCancelled | ParseError a String deriving Show
 
-parse :: Eq a => R.Reader b -> a -> [(a, Text)] -> Either (Err a) (b, Text)
-parse reader key aList =
+type Parser a = IR.IParser Text a
+
+parse :: Eq a => Parser b -> a -> [(a, Text)] -> Either (Err a) (b, Text)
+parse p key aList =
   case assoc key aList of
     Nothing -> Left FormCancelled
-    Just s  -> first (ParseError key) $ reader s
+    Just s  -> first (ParseError key) $ runP p s
+
+matchText :: Text -> R.Reader Text
+matchText prefix s =
+  if prefix `isPrefixOf` s
+  then Right (prefix, drop (length prefix) s)
+  else Left $ "Failed to match " <> show prefix
+
+takeAll :: R.Reader Text
+takeAll s = Right (s, "")
+
+bool :: R.Reader Bool
+bool s =
+  if s == ""
+  then Left "Empty string"
+  else
+    let (h, t) = (head s, tail s)
+    in case h of
+         '0' -> Right (False, t)
+         '1' -> Right (True, t)
+         _   -> Left "Failed to parse boolean"
+
+eos :: R.Reader ()
+eos s = if s == "" then Right ((), "") else Left "End-of-string expected"
 
 parseText :: Eq a => a -> Result a -> Either (Err a) Text
-parseText key aList = fst <$> parse (\s -> Right (s, "")) key aList
+parseText key aList = fst <$> parse (P takeAll) key aList
 
 parseFloat :: Eq a => a -> Result a -> Either (Err a) Float
-parseFloat key aList = fromRational . fst <$> parse R.rational key aList
+parseFloat key aList =
+  let p = IR.P R.rational <* IR.P eos
+  in fromRational . fst <$> parse p key aList
 
 parseBool :: Eq a => a -> Result a -> Either (Err a) Bool
-parseBool key aList = fst <$> parse readBool key aList
-  where
-    readBool :: R.Reader Bool
-    readBool s =
-      if s == ""
-      then Left "Empty string"
-      else
-        let (h, t) = (head s, tail s)
-        in case h of
-          '0' -> Right (False, t)
-          '1' -> Right (True, t)
-          _   -> Left "Failed to parse boolean"
+parseBool key aList =
+  let p = IR.P bool <* IR.P eos
+  in fst <$> parse p key aList
 
 parseInt :: Eq a => a -> Result a -> Either (Err a) Int
-parseInt key aList = fromIntegral . fst <$> parse R.decimal key aList
+parseInt key aList =
+  let p = IR.P R.decimal <* IR.P eos
+  in fromIntegral . fst <$> parse p key aList
 
 assoc :: Eq a => a -> [(a, b)] -> Maybe b
 assoc key xs = snd <$> find ((== key) . fst) xs
